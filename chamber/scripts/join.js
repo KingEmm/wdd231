a66ab457473a936e23955a24ecfca21c807633ad
kingemm <kinge7916@gmail.com>
2026-02-06 07:02:39 +0100

    Done for now

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -2,3 +2,11 @@ const time = document.querySelector('#time');
 
 time.value = Date.now()
 // alert(time.value)
+
+let nav_links = document.querySelectorAll('header nav a');
+
+nav_links.forEach(element => {
+    element.classList.remove('link')
+});
+
+nav_links[2].classList.add('link')
